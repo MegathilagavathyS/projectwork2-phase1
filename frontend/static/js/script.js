048ee5ef3b21429054b502a8fad5ee51f1ea75d5
pushpa <pushpa101005@gmail.com>
2026-07-27 21:18:53 +0530

    Add file

---
+++ frontend/static/js/script.js
@@ -0,0 +1,20 @@
+const imageInput = document.getElementById("imageInput");
+const preview = document.getElementById("preview");
+
+if(imageInput){
+
+    imageInput.addEventListener("change", function(){
+
+        const file = this.files[0];
+
+        if(file){
+
+            preview.style.display="block";
+
+            preview.src = URL.createObjectURL(file);
+
+        }
+
+    });
+
+}
